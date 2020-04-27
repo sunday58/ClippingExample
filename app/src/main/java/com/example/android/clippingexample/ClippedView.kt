3ad7da420e2ae4e227b6860831805b2374c9bd3a
sundaydavid <sundaydavid989@gmail.com>
2020-04-27 10:15:13 +0100

    clipping app

---
+++ app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -0,0 +1,232 @@
+package com.example.android.clippingexample
+
+import android.content.Context
+import android.graphics.*
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+
+class ClippedView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private val paint = Paint().apply {
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWith)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+
+    private val path = Path()
+
+    private val clipRectRights = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+
+    private val rectInset = resources.getDimension(R.dimen.rectInset)
+    private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+
+    private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
+
+    private val columnOne = rectInset
+    private val columnTwo = columnOne + rectInset + clipRectRights
+
+    private val rowOne = rectInset
+    private val rowTwo = rowOne + rectInset + clipRectBottom
+    private val rowThree = rowTwo + rectInset + clipRectBottom
+    private val rowFour = rowThree + rectInset + clipRectBottom
+    private val textRow = rowFour + (1.5f * clipRectBottom)
+
+    private var rectF = RectF(
+        rectInset,
+        rectInset,
+        clipRectRights - rectInset,
+        clipRectBottom - rectInset
+    )
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        drawBackAndUnclippedRectangle(canvas)
+        drawDifferenceClippingExample(canvas)
+        drawCircularClippingExample(canvas)
+        drawIntersectionClippingExample(canvas)
+        drawCombinedClippingExample(canvas)
+        drawRoundedRectangleClippingExample(canvas)
+        drawOutsideClippingExample(canvas) 
+        drawSkewedTextExample(canvas)
+        drawTranslatedTextExample(canvas)
+        drawQuickRejectExample(canvas)
+    }
+
+    private fun drawClippedRectangle(canvas: Canvas) {
+        canvas.clipRect(
+            clipRectLeft, clipRectTop,
+            clipRectRights, clipRectBottom
+        )
+        canvas.drawColor(Color.WHITE)
+        paint.color = Color.RED
+        canvas.drawLine(
+            clipRectLeft, clipRectTop,
+            clipRectRights, clipRectBottom, paint
+        )
+        paint.color = Color.GREEN
+        canvas.drawCircle(
+            circleRadius, clipRectBottom - circleRadius,
+            circleRadius, paint
+        )
+        paint.color = Color.BLUE
+        paint.textSize = textSize
+        paint.textAlign = Paint.Align.RIGHT
+        canvas.drawText(
+            context.getString(R.string.clipping),
+            clipRectRights, textOffset, paint
+        )
+    }
+
+    private fun drawQuickRejectExample(canvas: Canvas?) {
+
+    }
+
+    private fun drawRoundedRectangleClippingExample(canvas: Canvas?) {
+        canvas!!.save()
+        path.rewind()
+        path.addRoundRect(
+            rectF, clipRectRights / 4,
+            clipRectRights / 4, Path.Direction.CCW
+        )
+        canvas.clipPath(path)
+        drawClippedRectangle(canvas)
+        canvas.restore()
+    }
+
+    private fun drawDifferenceClippingExample(canvas: Canvas?) {
+
+        canvas!!.save()
+        canvas.translate(columnTwo, rowOne)
+        canvas.clipRect(
+            2 * rectInset, 2 * rectInset,
+            clipRectRights - 2 * rectInset,
+            clipRectBottom - 2 * rectInset
+        )
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+            canvas.clipRect(
+                4 * rectInset, 4 * rectInset,
+                clipRectRights - 4 * rectInset,
+                clipRectBottom - 4 * rectInset,
+                Region.Op.DIFFERENCE
+            )
+        else {
+            canvas.clipOutRect(
+                4 * rectInset, 4 * rectInset,
+                clipRectRights - 4 * rectInset,
+                clipRectBottom - 4 * rectInset
+            )
+        }
+        drawClippedRectangle(canvas)
+        canvas.restore()
+    }
+
+    private fun drawBackAndUnclippedRectangle(canvas: Canvas?) {
+        canvas!!.drawColor(Color.GRAY)
+        canvas.save()
+        canvas.translate(columnOne, rowOne)
+        drawClippedRectangle(canvas)
+        canvas.restore()
+    }
+    private fun  drawCircularClippingExample(canvas: Canvas?) {
+        canvas!!.save()
+        canvas.translate(columnOne, rowTwo)
+
+        path.rewind()
+        path.addCircle(
+            circleRadius, clipRectBottom - circleRadius,
+            circleRadius, Path.Direction.CCW
+        )
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
+            canvas.clipPath(path, Region.Op.DIFFERENCE)
+        }else{
+            canvas.clipOutPath(path)
+        }
+        drawClippedRectangle(canvas)
+        canvas.restore()
+    }
+    private fun  drawIntersectionClippingExample(canvas: Canvas?) {
+        canvas!!.save()
+        canvas.translate(columnTwo, rowTwo)
+        canvas.clipRect(
+            clipRectLeft, clipRectTop,
+            clipRectRights - smallRectOffset,
+            clipRectBottom - smallRectOffset
+        )
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
+            canvas.clipRect(
+                clipRectLeft + smallRectOffset,
+                clipRectTop + smallRectOffset,
+                clipRectRights, clipRectBottom,
+                Region.Op.INTERSECT
+            )
+        } else{
+            canvas.clipRect(
+                clipRectLeft + smallRectOffset,
+                clipRectTop + smallRectOffset,
+                clipRectRights, clipRectBottom
+            )
+        }
+        drawClippedRectangle(canvas)
+        canvas.restore()
+    }
+    private fun   drawCombinedClippingExample(canvas: Canvas?) {
+        canvas!!.save()
+        canvas.translate(columnOne, rowThree)
+        path.rewind()
+        path.addCircle(
+            clipRectLeft + rectInset + circleRadius,
+            clipRectTop + circleRadius + rectInset,
+            circleRadius, Path.Direction.CCW
+        )
+        path.addRect(
+            clipRectRights / 2 - circleRadius,
+            clipRectTop + circleRadius + rectInset,
+            clipRectRights / 2 + circleRadius,
+            clipRectBottom - rectInset, Path.Direction.CCW
+        )
+        canvas.clipPath(path)
+        drawClippedRectangle(canvas)
+        canvas.restore()
+    }
+    private fun   drawOutsideClippingExample(canvas: Canvas?) {
+        canvas!!.save()
+        canvas.translate(columnOne, rowFour)
+        canvas.clipRect(2 * rectInset, 2 * rectInset,
+        clipRectRights - 2 * rectInset,
+        clipRectBottom - 2 * rectInset)
+        drawClippedRectangle(canvas)
+        canvas.restore()
+    }
+    private fun   drawSkewedTextExample(canvas: Canvas?) {
+        canvas!!.save()
+        paint.color = Color.YELLOW
+        paint.textAlign = Paint.Align.RIGHT
+        canvas.translate(columnTwo, textRow)
+        canvas.skew(0.2f, 0.3f)
+        canvas.drawText(context.getString(R.string.skewed),
+        clipRectLeft, clipRectTop, paint)
+        canvas.restore()
+    }
+    private fun   drawTranslatedTextExample(canvas: Canvas?) {
+        canvas!!.save()
+        paint.color = Color.GREEN
+        paint.textAlign = Paint.Align.LEFT
+        canvas.translate(
+            columnTwo, textRow
+        )
+        canvas.drawText(context.getString(R.string.translated),
+        clipRectLeft, clipRectTop, paint)
+        canvas.restore()
+    }
+}
